7bf8c9a11854a71334a8d6356519e9fcbd822b05
Ireneusz W Bulik <ireneusz.bulik@gmail.com>
2017-07-04 20:19:43 -0400

    fix 265

--- 265.f95
+++ 265.f95
@@ -1,6 +1,10 @@
-! more than a minute, but ok for now, with O3 we have 90 sec.
+! Probably can make it even faster. 
 !
 ! Answer =          209110240768
+!
+!real    0m33.275s
+!user    0m32.982s
+!sys     0m0.053s
 !
 ! I thought we would have issue with the rotational clashes, but seems we do not. 
 ! I can probably take it under minute with some effort.
@@ -16,6 +20,7 @@
            call dec_to_bin(i,arr,2**big_n) 
            if(is_unique(big_n,arr,vals,dig)) then
              tot = tot + i
+             print *, i 
            endif 
         enddo 
         print *, 'Answer = ' , tot 
@@ -57,23 +62,24 @@
         end subroutine 
 
         logical function is_unique(n,arr,vals,dig)
-        integer      :: n,arr(2**n),vals(2**n),dig(n)  
-        integer      :: i,j
+        integer      :: n,arr(2**n),vals(0:2**n-1),dig(n)  
+        integer      :: i,j,num
 
+        vals = 0 
+        
+        is_unique = .true.
 
         do i = 1 , 2**n 
+           num = 0 
            do j = 1 , n
-           dig(j) = arr(cyc_index(i+j-1,2**n))
+           num = num + arr(cyc_index(i+j-1,2**n))*2**(n-j)
            enddo 
-           vals(i) = bin_to_dec(n,dig)
-        enddo 
-
-        call sort(vals,2**n)
-
-        is_unique = .true.
-        do i = 2 , 2**n
-           if(vals(i).eq.vals(i-1)) is_unique = .false.
-           if(.not.is_unique) return
+           if(vals(num).eq.1) then
+             is_unique=.false.
+             return 
+           else 
+             vals(num) = 1 
+           endif 
         enddo 
 
         return
@@ -88,62 +94,6 @@
         return
         end 
 
-        subroutine sort(a,n)
-        implicit none
-        integer  :: a(*),n
-        integer  :: ne,nb,bc,w,b
-! HIRING PART. WE START WITH N/2+1 HIRED PEOPLE AND WE EMPLOY POTENTIAL 
-! N/2 BOSSED
-        ne = n
-        nb = n/2+1
-        if(n.le.1) return 
-200     continue         
-        if(nb.gt.1) then 
-! HIRE GUY AND THEN ASSES HIM            
-           nb = nb - 1 
-           bc = a(nb)
-        else 
-! RETIRE THE TOP GUY, TAKE THE LAST ONE AND GIVE HIM THE PLACE. THEN
-! VERIFY  
-           bc    = a(ne)
-           a(ne) = a(1) 
-           ne    = ne - 1 
-             if(ne.eq.1) then 
-                a(1) = bc 
-                return
-             endif 
-!
-         endif 
-! SET AS THE BOSS THE ITH ELEMENTS 
-        b = nb 
-        w = 2 * b 
-! IN THE HIRE LOOP, FOR BOSS B WE MAKE SURE THAT NO WORKER W
-! SUPERVISED BY B HAS BETTER QUALIFICATIONS. THE WORKERS ARE 
-! 2*B AND 2*B + 1 . IS THE SWITCH IS DONE, THEN WE NEED TO MAKE SURE 
-! NO MORE LEVELS OF DEMOTION ARE NEEDED. BOOS QUALIFICATIONS ARE BC.
-! THERE ARE NE PEOPLE.
-100      continue 
-         if(w+1.le.ne) then 
-           if(a(w+1).gt.a(w)) w = w + 1 
-         endif 
-         if(bc.lt.a(w)) then 
-            a(b) = a(w)         
-! CHECK IF YOU NEED TO DEMOTE B FURTHER                    
-            b    = w
-            w    = 2 * b 
-         else 
-! YOU DON'T, SKIP TO THE LAST EMPLOYEE 
-            w = ne + 1 
-         endif 
-         if(w.le.ne) goto 100 
-! NO MORE DEMOTION IS NEEDED. PUT OLD BOSS QUALIFICATIONS INTO HIS NEW
-! POSITION 
-         a(b) = bc 
-
-         goto 200 
-
-         return
-         end subroutine 
 
 
         end program 
