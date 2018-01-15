367863b66e343f6750921107c56602ddc3f6e043
Ireneusz Bulik <irek@pylon.local>
2018-01-14 22:41:18 -0500

    little change to 618

--- 618.cpp
+++ 618.cpp
@@ -14,12 +14,12 @@
  *   Organization:  
  *
  * =====================================================================================
- */
- //[0] g++ -O3 -o 618.x 618.cpp ; time ./618.x
-//Answer 618 = 634212216
-//real	0m17.242s
-//user	0m15.340s
-//sys	0m1.116s
+ [0] g++ -O3 -o 618.x 618.cpp; time  ./618.x
+Answer 618 = 634212216
+real	0m12.805s
+user	0m11.975s
+sys	0m0.764s
+*/
 
 #include <iostream>
 #include <vector>
@@ -41,34 +41,34 @@ void get_primes(vector<int> &p,int n){
   return;
 }
 
-long long psfib(const vector<int> &p, int s, int sum, long long psf, vector<vector<long long>> &memo){
-  long long mod=1000000000LL; 
+long long psfib(const vector<int> &p, int s, int sum, vector<vector<long long>> &memo){
+  long mod=1000000000L; 
   if(memo[sum][s]!=-1)
-    return (psf*memo[sum][s])%mod;
+    return memo[sum][s]%mod;
   if(sum==0)
-    return psf%mod;
+    return 1LL;
   if(s>=p.size()||p[s]>sum||sum<0)
     return 0LL;
-  long long key1 = psfib(p,s,sum-p[s],1,memo); 
+  long long key1 = psfib(p,s,sum-p[s],memo); 
   if(sum-p[s]>=0) 
     memo[sum-p[s]][s]=key1%mod;
-  long long key2 = psfib(p,s+1,sum,1,memo);
+  long long key2 = psfib(p,s+1,sum,memo);
   if(s+1<p.size())
     memo[sum][s+1]=key2%mod;
-  return (key1*psf*p[s]+key2*psf)%mod;
+  return (key1*p[s]+key2)%mod;
 }
 
 
 int main(){
   vector<int> fib(24);
   fib[0]=1,fib[1]=1;
-  for(int i=2;i<24;fib[i]=fib[i-1]+fib[i-2],i++);
+  for(int i=2;i<fib.size();fib[i]=fib[i-1]+fib[i-2],i++);
   vector<int> primes;
-  get_primes(primes,fib[23]);
+  get_primes(primes,fib.back());
   long long ans =0LL;
-  vector<vector<long long>> memo(1+fib[23],vector<long long>(primes.size(),-1));
-  for(int i=1;i<24;i++){
-    ans+=psfib(primes,0,fib[i],1,memo);
+  vector<vector<long long>> memo(1+fib.back(),vector<long long>(primes.size(),-1));
+  for(int i=1;i<fib.size();i++){
+    ans+=psfib(primes,0,fib[i],memo);
   }
   cout << "Answer 618 = " << ans%1000000000;     
   return 0;
